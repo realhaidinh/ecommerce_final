0ca7c50ee0ede64893fa4864fd3ff175a99b7495
realhaidinh <dinhnguyenminhhai@gmail.com>
2025-04-21 20:37:01 +0700

    added product image schema

--- lib/ecommerce_final/catalog/product.ex
+++ lib/ecommerce_final/catalog/product.ex
@@ -7,15 +7,22 @@ defmodule EcommerceFinal.Catalog.Product do
     field :title, :string
     field :price, :integer
     field :stock, :integer
-    field :sold, :integer
+    field :sold, :integer, default: 0
 
+    many_to_many :categories, EcommerceFinal.Catalog.Category,
+      join_through: "product_categories",
+      on_replace: :delete,
+      preload_order: [asc: :level]
+
+    has_many :images, EcommerceFinal.Catalog.ProductImage, on_replace: :delete
     timestamps(type: :utc_datetime)
   end
 
   @doc false
   def changeset(product, attrs) do
     product
-    |> cast(attrs, [:title, :description, :price, :stock, :sold])
-    |> validate_required([:title, :description, :price, :stock, :sold])
+    |> cast(attrs, [:title, :description, :price, :stock])
+    |> validate_required([:title, :description, :price, :stock])
+    |> validate_number(:price, greater_than: 0)
   end
 end

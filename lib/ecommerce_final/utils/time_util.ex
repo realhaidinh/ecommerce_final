5a22921afcb56d1515d63e37718d588d4b82f7c4
realhaidinh <dinhnguyenminhhai@gmail.com>
2025-06-08 19:12:44 +0700

    added utils

---
+++ lib/ecommerce_final/utils/time_util.ex
@@ -0,0 +1,17 @@
+defmodule EcommerceFinal.Utils.TimeUtil do
+  def pretty_print(date = %DateTime{}) do
+    {:ok, date} = DateTime.shift_zone(date, "Asia/Saigon")
+    Calendar.strftime(date, "%d/%m/%Y")
+  end
+
+  def pretty_print_with_time(date = %DateTime{}) do
+    {:ok, date} = DateTime.shift_zone(date, "Asia/Saigon")
+    Calendar.strftime(date, "%d/%m/%Y %H:%M:%S")
+  end
+
+  @epoch DateTime.from_unix!(0)
+  def get_current_time() do
+    {:ok, time} = DateTime.now("Asia/Saigon")
+    DateTime.diff(time, @epoch)
+  end
+end
